0bd7b048521e76ab5e193e1c1136354a9738b9fe
jorge <jlguevara81@gmail.com>
2016-07-19 13:45:50 -0700

    changed the name of the game engine

---
+++ game-engine/settings.js
@@ -0,0 +1,36 @@
+/**
+ * So far images are assumed to be png and sounds mp3
+ */
+
+var settings = {
+    goal: 5,
+
+    assetsDirectory: 'balloon',
+
+    background: 'deepblue',
+
+    images: [   'balloonA', 
+                'balloonB', 
+                'balloonC', 
+                'balloonD', 
+                'balloonE'
+    ],
+
+    goalImage: 'balloonA',
+
+    goalImageProbability: 0.3,
+
+    popImage : 'balloon-pop',
+
+    popSound : 'pop',
+
+    popLifeSpan: 800,
+
+    delay : 1500,
+
+    velocity: -100,
+
+    winMessage: 'You win!',
+
+    winSound : 'youWin',
+}
