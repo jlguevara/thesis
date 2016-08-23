d55551010d93c8c2c1662a937159023222acb3a3
jlguevara <jlguevara81@gmail.com>
2016-08-23 01:26:07 -0700

    updated fixPermissions

--- fixPermisions.sh
+++ fixPermisions.sh
@@ -2,13 +2,13 @@
 
 chmod o+r index.html
 
-chmod -R o+rx game-engine
-chmod o+r game-engine/index.html
-chmod o+r game-engine/getSettings.js
-chmod o+r game-engine/game.js
-chmod -R o+rx game-engine/settings
-chmod -R o+rx game-engine/images/
-chmod -R o+rx game-engine/sounds/
+chmod -R o+rx pop-objects
+chmod o+r pop-objects/index.html
+chmod o+r pop-objects/getSettings.js
+chmod o+r pop-objects/game.js
+chmod -R o+rx pop-objects/settings
+chmod -R o+rx pop-objects/images/
+chmod -R o+rx pop-objects/sounds/
 
 chmod -R o+rx avoid-objects
 chmod o+r avoid-objects/index.html
