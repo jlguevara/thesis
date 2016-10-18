69d057a1f1a140ce32c9c8ca29853f9f27f8de72
jorge <jlguevara81@gmail.com>
2016-10-18 14:25:06 -0700

    spanish sounds added

---
+++ pop-objects/sounds/makeSoundsSpanish.sh
@@ -0,0 +1,81 @@
+say -o tmp por favor revienta el globo con la letra a
+lame tmp.aiff balloonA-spanish.mp3
+
+# bug with letter b in spanish
+say -o tmp por favor revienta el globo con la letra be
+lame tmp.aiff balloonB-spanish.mp3
+
+say -o tmp por favor revienta el globo con la letra c
+lame tmp.aiff balloonC-spanish.mp3
+say -o tmp por favor revienta el globo con la letra d
+lame tmp.aiff balloonD-spanish.mp3
+say -o tmp por favor revienta el globo con la letra e
+lame tmp.aiff balloonE-spanish.mp3
+say -o tmp por favor revienta el globo con la letra f
+lame tmp.aiff balloonF-spanish.mp3
+say -o tmp por favor revienta el globo con la letra g
+lame tmp.aiff balloonG-spanish.mp3
+say -o tmp por favor revienta el globo con la letra h
+lame tmp.aiff balloonH-spanish.mp3
+say -o tmp por favor revienta el globo con la letra i
+lame tmp.aiff balloonI-spanish.mp3
+say -o tmp por favor revienta el globo con la letra j
+lame tmp.aiff balloonJ-spanish.mp3
+say -o tmp por favor revienta el globo con la letra k
+lame tmp.aiff balloonK-spanish.mp3
+say -o tmp por favor revienta el globo con la letra l
+lame tmp.aiff balloonL-spanish.mp3
+say -o tmp por favor revienta el globo con la letra m
+lame tmp.aiff balloonM-spanish.mp3
+say -o tmp por favor revienta el globo con la letra n
+lame tmp.aiff balloonN-spanish.mp3
+say -o tmp por favor revienta el globo con la letra o
+lame tmp.aiff balloonO-spanish.mp3
+say -o tmp por favor revienta el globo con la letra p
+lame tmp.aiff balloonP-spanish.mp3
+say -o tmp por favor revienta el globo con la letra q
+lame tmp.aiff balloonQ-spanish.mp3
+say -o tmp por favor revienta el globo con la letra r
+lame tmp.aiff balloonR-spanish.mp3
+say -o tmp por favor revienta el globo con la letra s
+lame tmp.aiff balloonS-spanish.mp3
+say -o tmp por favor revienta el globo con la letra t
+lame tmp.aiff balloonT-spanish.mp3
+say -o tmp por favor revienta el globo con la letra u
+lame tmp.aiff balloonU-spanish.mp3
+say -o tmp por favor revienta el globo con la letra v
+lame tmp.aiff balloonV-spanish.mp3
+say -o tmp por favor revienta el globo con la letra w
+lame tmp.aiff balloonW-spanish.mp3
+say -o tmp por favor revienta el globo con la letra x
+lame tmp.aiff balloonX-spanish.mp3
+say -o tmp por favor revienta el globo con la letra y
+lame tmp.aiff balloonY-spanish.mp3
+say -o tmp por favor revienta el globo con la letra z
+lame tmp.aiff balloonZ-spanish.mp3
+
+
+say -o tmp una vez 
+lame tmp.aiff 1-spanish.mp3
+say -o tmp dos veces 
+lame tmp.aiff 2-spanish.mp3
+say -o tmp tres veces 
+lame tmp.aiff 3-spanish.mp3
+say -o tmp cuatro veces 
+lame tmp.aiff 4-spanish.mp3
+say -o tmp cinco veces 
+lame tmp.aiff 5-spanish.mp3
+say -o tmp seis veces 
+lame tmp.aiff 6-spanish.mp3
+say -o tmp siete veces 
+lame tmp.aiff 7-spanish.mp3
+say -o tmp ocho veces 
+lame tmp.aiff 8-spanish.mp3
+say -o tmp nueve veces 
+lame tmp.aiff 9-spanish.mp3
+say -o tmp diez veces 
+lame tmp.aiff 10-spanish.mp3
+
+
+say -o tmp felicitaciones, tu ganas
+lame tmp.aiff win-spanish.mp3
